ade1f8081139b8093df1074b6498a3d9ce2504dc
Sejal-patel <sejal9992@gmail.com>
2025-07-01 22:21:11 +0530

    added additional code in tools.py and requirements.txt

---
+++ toolkit/tools.py
@@ -0,0 +1,20 @@
+from langchain.tools import tool
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from lancedb.rerankers import LinearCombinationReranker
+from typing import List, Optional
+
+
+# create rag tool
+
+
+# tavily tool
+
+
+# polygon tool
+
+
+# bing tool
+
+
+# get all tools
+
